1cf506111b432836a70fe25261fc8463b0b6f091
chetan <chetanpaliwalcool@gmail.com>
2017-09-17 02:09:39 +0530

    Halfway done with new feature. Tested so far, so good.

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
+function aNewBigSomething () {
+	console.log('Writing a lot of code');
+	console.log('Halfway done with the IMAD course');
+};
+
 function something() {
 	console.log('I\'n something');
 }
@@ -5,3 +10,4 @@ function something() {
 console.log('hello world');
 
 something();
+aNewBigSomething();
